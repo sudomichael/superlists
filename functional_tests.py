590bf5ead87cad01b3cb72d4a4f2e38b3e6d5d52
sudomichael <devmpk@gmail.com>
2016-10-18 15:53:17 -0400

    Made selenium close browser after opening it, reformatted test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
+import unittest 
+#browser = webdriver.Chrome('/usr/bin/chromedriver')
 
-browser = webdriver.Chrome('/usr/bin/chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+    def setUp(self):
+        self.browser = webdriver.Chrome('/usr/bin/chromedriver')
+        self.browser.implicitly_wait(3)
+ 
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
