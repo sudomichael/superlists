541d66cc5134180c1455ef8d79c7abf8b1b1cd48
sudomichael <devmpk@gmail.com>
2016-10-21 11:28:48 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest 
 #browser = webdriver.Chrome('/usr/bin/chromedriver')
 
@@ -12,8 +14,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # navigate to web page
         self.browser.get('http://localhost:8000')
+        
+        # test if "To-Do" is in page title
         self.assertIn('To-Do', self.browser.title)
+
+        # Get h1 element, see if "To-Do" is within the tag
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Get id_new_item element, 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'        )
+
+        #
+        inputbox.send_keys('Buy peacock feathers')
+
+        #
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
